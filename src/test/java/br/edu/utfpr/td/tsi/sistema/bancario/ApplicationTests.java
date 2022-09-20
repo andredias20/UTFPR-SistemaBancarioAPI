19899f798cca8618abdf59465d9189570178d1f7
André De Carli Dias <andre.c.dias2000@gmail.com>
2022-09-19 23:57:36 -0300

    Starting with spring starter

---
+++ src/test/java/br/edu/utfpr/td/tsi/sistema/bancario/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.sistema.bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
